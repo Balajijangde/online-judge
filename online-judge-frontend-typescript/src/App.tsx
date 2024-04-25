897b819df24b4ddad7b8f19da11f3d480edfa431
Balaji Jangde <balajangde@gmail.com>
2024-04-26 00:59:21 +0530

    bug: Use HashRouter instead of BrowserRouter

--- online-judge-frontend-typescript/src/App.tsx
+++ online-judge-frontend-typescript/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
